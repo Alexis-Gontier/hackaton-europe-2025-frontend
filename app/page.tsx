b6d08637ae5c434eb597aa9e1902e948d6267814
Gontier <alexis.gontier03@gmail.com>
2025-03-17 02:19:48 +0100

    Ajout de la gestion des tokens d'accès avec des boutons pour ajouter et enlever le token

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+"use client"
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [message, setMessage] = useState('');
+
+  const addToken = () => {
+    localStorage.setItem('access_token', 'Hello, this is a message!');
+    setMessage('ajouté');
+  };
+
+  const removeToken = () => {
+    localStorage.removeItem('access_token');
+    setMessage('enlevé');
+  };
+
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to the home page!</p>
+      <button onClick={addToken}>Add Token</button>
+      <button onClick={removeToken}>remove Token</button>
+      <p>{message}</p>
     </div>
   );
 }
