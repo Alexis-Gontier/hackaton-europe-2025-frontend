32fe8374efb5b5c8a3991459c64835b011b8c1be
Alexis G <alexis.gontier03@gmail.com>
2025-03-18 12:41:25 +0100

    ✨ temporary push

--- components/scroll/HorizontalCarousel.tsx
+++ components/scroll/HorizontalCarousel.tsx
@@ -1,13 +1,35 @@
+"use client"
+
+import * as React from "react"
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
+    type CarouselApi,
 } from "@/components/ui/carousel"
 import CardInfo from "./CardInfo"
 
 export default function HorizontalCarousel() {
+
+    const [api, setApi] = React.useState<CarouselApi>()
+    const [current, setCurrent] = React.useState(0)
+    const [count, setCount] = React.useState(0)
+
+    React.useEffect(() => {
+        if (!api) {
+          return
+        }
+
+        setCount(api.scrollSnapList().length)
+        setCurrent(api.selectedScrollSnap() + 1)
+
+        api.on("select", () => {
+          setCurrent(api.selectedScrollSnap() + 1)
+        })
+    }, [api])
+
   return (
-    <Carousel>
+    <Carousel className="relative w-full h-full" onApi={setApi}>
         <CarouselContent>
             <CarouselItem>
                 <CardInfo />
@@ -21,6 +43,9 @@ export default function HorizontalCarousel() {
             <CarouselItem>
                 <CardInfo />
             </CarouselItem>
+            <div className="fixed bottom-0 right-0 p-4 bg-white bg-opacity-50">
+                {current} / {count}
+            </div>
         </CarouselContent>
     </Carousel>
   )
