708eedd96d433f256565c60b84736f71aa3fad3a
Alexis G <alexis.gontier03@gmail.com>
2025-03-18 13:00:52 +0100

    🚀 Refactor HorizontalCarousel to remove unused state and API handling

--- components/scroll/HorizontalCarousel.tsx
+++ components/scroll/HorizontalCarousel.tsx
@@ -1,35 +1,14 @@
-"use client"
-
-import * as React from "react"
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
-    type CarouselApi,
 } from "@/components/ui/carousel"
 import CardInfo from "./CardInfo"
 
 export default function HorizontalCarousel() {
 
-    const [api, setApi] = React.useState<CarouselApi>()
-    const [current, setCurrent] = React.useState(0)
-    const [count, setCount] = React.useState(0)
-
-    React.useEffect(() => {
-        if (!api) {
-          return
-        }
-
-        setCount(api.scrollSnapList().length)
-        setCurrent(api.selectedScrollSnap() + 1)
-
-        api.on("select", () => {
-          setCurrent(api.selectedScrollSnap() + 1)
-        })
-    }, [api])
-
   return (
-    <Carousel className="relative w-full h-full" onApi={setApi}>
+    <Carousel className="relative w-full h-full">
         <CarouselContent>
             <CarouselItem>
                 <CardInfo />
@@ -43,9 +22,6 @@ export default function HorizontalCarousel() {
             <CarouselItem>
                 <CardInfo />
             </CarouselItem>
-            <div className="fixed bottom-0 right-0 p-4 bg-white bg-opacity-50">
-                {current} / {count}
-            </div>
         </CarouselContent>
     </Carousel>
   )
