6c2cb7a56720206109e807b2dcfd3cb87be160a9
Alexis G <alexis.gontier03@gmail.com>
2025-03-19 00:26:27 +0100

    ✨ Add navbaer

--- app/(article11)/test/page.tsx
+++ app/(article11)/test/page.tsx
@@ -1,30 +1,9 @@
-'use client';
+import React from 'react'
 
-import { useEffect, useState } from 'react';
-import useAuth from '@/hooks/useAuth';
-import localforage from 'localforage';
-
-export default function Page() {
-  const [token, setToken] = useState<string | null>(null);
-  const { logout } = useAuth();
-
-  useEffect(() => {
-    async function fetchToken() {
-      const t = await localforage.getItem<string>('access_token')
-      setToken(t)
-    }
-
-    fetchToken()
-  }, [])
-
-  function handleLogout() {
-    logout()
-  }
-
+export default function page() {
   return (
     <div>
-      <p>Token: {token}</p>
-      <button onClick={handleLogout}>Logout</button>
+      Page test
     </div>
-  );
+  )
 }
