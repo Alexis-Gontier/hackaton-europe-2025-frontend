60300eb53614a6b1e131bb98ab707a447b9228ff
Alexis G <alexis.gontier03@gmail.com>
2025-03-19 09:04:52 +0100

    🔥 Remove the carousel page component

--- app/(article11)/carousel/page.tsx
+++
@@ -1,9 +0,0 @@
-import VerticalCarousel from "@/components/scroll/VerticalCarousel"
-
-export default function page() {
-  return (
-    <div className="max-w-96 overflow-hidden m-auto max-h-screen">
-      <VerticalCarousel />
-    </div>
-  )
-}
