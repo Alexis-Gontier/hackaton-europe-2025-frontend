7b6c82ebf73422ef46a06de68dea0c470e360091
Gontier <alexis.gontier03@gmail.com>
2025-03-20 00:52:30 +0100

    ✨ Refactor LoginPage component by integrating LoginForm and simplifying structure

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,60 +1,11 @@
-"use client";
-
-import { useState } from "react";
-import useAuth from "@/hooks/useAuth";
-import Link from "next/link";
+import LoginForm from "@/components/auth/LoginForm"
 
 export default function LoginPage() {
-  const { login, loading, error } = useAuth();
-  const [loginData, setLoginData] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleLoginChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setLoginData({
-      ...loginData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await login(loginData.username, loginData.password);
-  };
-
   return (
-    <div>
-      <h1>Connexion</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Nom d'utilisateur"
-          value={loginData.username}
-          onChange={handleLoginChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-          value={loginData.password}
-          onChange={handleLoginChange}
-          required
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Chargement..." : "Se connecter"}
-        </button>
-      </form>
-      <p>
-        Pas de compte ? <Link href="/register">Inscrivez-vous</Link>
-      </p>
-      <Link href={"/privacy"}>
-        Privacy Policy
-      </Link>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
     </div>
-  );
+  )
 }
