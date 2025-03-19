ce1dcc6122490417dac0d71523ebf4c64776aa0b
Alexis G <alexis.gontier03@gmail.com>
2025-03-19 09:01:24 +0100

    🔥  fixe

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-"use client"
+import VerticalCarousel from "@/components/scroll/VerticalCarousel"
 
-import localforage from 'localforage';
-import { useState } from 'react';
-import Link from 'next/link';
-
-export default function Home() {
-  const [message, setMessage] = useState('');
-
-  async function addToken() {
-    await localforage.setItem('access_token', 'Hello, this is a message!')
-    setMessage('ajouté')
-  }
-
-  async function removeToken() {
-    await localforage.removeItem('access_token')
-    setMessage('enlevé')
-  }
-
+export default function page() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-      <button onClick={addToken}>Add Token</button>
-      <button onClick={removeToken}>remove Token</button>
-      <p>{message}</p>
-
-
-      <p>Link Page:<Link href="/carousel">Carousel</Link></p>
+    <div className="max-w-96 overflow-hidden m-auto max-h-screen">
+      <VerticalCarousel />
     </div>
-  );
+  )
 }
