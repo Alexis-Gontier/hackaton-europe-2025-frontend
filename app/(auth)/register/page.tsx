e4cfc1ab6a6378603033c7a0e57546edd2738413
Gontier <alexis.gontier03@gmail.com>
2025-03-20 00:52:38 +0100

    ✨ Refactor RegisterPage component by integrating RegisterForm and simplifying structure

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,98 +1,11 @@
-"use client";
-
-import { useState } from "react";
-import useAuth from "@/hooks/useAuth";
-import Link from "next/link";
+import RegisterForm from "@/components/auth/RegisterForm"
 
 export default function RegisterPage() {
-  const { register, loading, error } = useAuth();
-  const [registerData, setRegisterData] = useState({
-    username: "",
-    firstname: "",
-    name: "",
-    email: "",
-    password: "",
-    consent: false,
-    role: "user",
-  });
-
-  const handleRegisterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target;
-    setRegisterData({
-      ...registerData,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await register(registerData);
-  };
-
   return (
-    <div>
-      <h1>Inscription</h1>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Nom d&apos;utilisateur"
-          value={registerData.username}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          type="text"
-          name="firstname"
-          placeholder="Prénom"
-          value={registerData.firstname}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          type="text"
-          name="name"
-          placeholder="Nom"
-          value={registerData.name}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={registerData.email}
-          onChange={handleRegisterChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-          value={registerData.password}
-          onChange={handleRegisterChange}
-          required
-        />
-        <label>
-          <input
-            type="checkbox"
-            name="consent"
-            checked={registerData.consent}
-            onChange={handleRegisterChange}
-          />
-          J&apos;accepte les conditions d&apos;utilisation
-        </label>
-        <button type="submit" disabled={loading}>
-          {loading ? "Chargement..." : "S&apos;inscrire"}
-        </button>
-      </form>
-      <p>
-        Vous avez déjà un compte ? <Link href="/login">Connectez-vous</Link>
-      </p>
-      <Link href={"/privacy"}>
-        Privacy Policy
-      </Link>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <RegisterForm />
+      </div>
     </div>
-  );
+  )
 }
