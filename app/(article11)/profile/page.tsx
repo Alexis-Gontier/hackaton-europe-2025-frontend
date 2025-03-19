9009fc20cc03b0edeb1d8f0982edbfd0df6c4586
Gontier <alexis.gontier03@gmail.com>
2025-03-19 23:29:28 +0100

    ✨ merge main

--- app/(article11)/profile/page.tsx
+++ app/(article11)/profile/page.tsx
@@ -26,9 +26,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import useAuth from "@/hooks/useAuth";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
 
 export default function AccordionAccount() {
-export default function CardWithForm() {
+
   const [gender, setGender] = React.useState("male");
 
   const {  logout } = useAuth();
