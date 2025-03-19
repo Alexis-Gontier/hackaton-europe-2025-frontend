c273e849dd2927ad99dfb3e22cc521f10b76fcc9
Gontier <alexis.gontier03@gmail.com>
2025-03-20 00:52:00 +0100

    ✨Improved search bar with space and placeholder

--- app/(article11)/search/page.tsx
+++ app/(article11)/search/page.tsx
@@ -4,8 +4,11 @@ import { Input } from "@/components/ui/input"
 
 export default function page() {
   return (
-    <div>
-      <Input className='rounded-full border-black'/>
+    <div className="px-4 flex items-center gap-2">
+      <Input
+        placeholder='Search'
+        className='rounded-full border-1 border-black'
+      />
     </div>
   )
 }
