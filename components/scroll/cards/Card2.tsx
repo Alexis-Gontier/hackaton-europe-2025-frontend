7f26f67b405511ea731384edbeec5649120e9330
Alexis G <alexis.gontier03@gmail.com>
2025-03-20 18:41:44 +0100

    ✨ Added shadows to map components to improve visibility and aesthetics

--- components/scroll/cards/Card2.tsx
+++ components/scroll/cards/Card2.tsx
@@ -6,11 +6,11 @@ import Link from "next/link";
 export default function Card2() {
   return (
     <Card
-      className="relative w-full h-[calc(100vh-200px)] rounded-4xl bg-clip-padding bg-gradient-to-b from-[#00ff3c] to-[#1f9900] p-2"
+      className="relative w-full h-[calc(100vh-200px)] rounded-4xl bg-clip-padding bg-gradient-to-b from-[#00ff3c] to-[#1f9900] p-2 after:content-[''] after:absolute after:inset-0 after:rounded-4xl after:shadow-[0_0_10px_5px_rgba(0,255,60,0.5)]"
     >
       <div className="w-full h-full rounded-3xl bg-white p-6 flex flex-col justify-center items-center text-center gap-4">
-        <h2 className="text-4xl font-extrabold after:content-[''] after:mt-3 after:block after:w-full after:h-1 after:bg-gradient-to-b after:from-[#00ff3c] after:to-[#1f9900] after:blur-[2px]">CONTEXT</h2>
-        <p className="font-semibold">In response to escalating geopolitical tensions and security concerns, EU leaders have agreed to significantly increase defense expenditures. This move aims to enhance the EUs autonomous defense capabilities and reduce reliance on external powers.</p>
+      <h2 className="text-4xl font-extrabold after:content-[''] after:mt-3 after:block after:w-full after:h-1 after:bg-gradient-to-b after:from-[#00ff3c] after:to-[#1f9900] after:blur-[2px]">CONTEXT</h2>
+      <p className="font-semibold">In response to escalating geopolitical tensions and security concerns, EU leaders have agreed to significantly increase defense expenditures. This move aims to enhance the EUs autonomous defense capabilities and reduce reliance on external powers.</p>
       </div>
       <p className="absolute bottom-6 left-6">Source: <Link href={"https://www.theguardian.com/europe"} target="_blank" className="blur-[.5px] italic text-blue-500 underline">The Guardian</Link></p>
     </Card>
--- components/scroll/cards/Card3.tsx
+++ components/scroll/cards/Card3.tsx
@@ -7,26 +7,26 @@ import Link from "next/link";
 export default function Card3() {
   return (
     <Card
-      className="relative w-full h-[calc(100vh-200px)] border-6 border-red-400 rounded-4xl"
+      className="relative w-full h-[calc(100vh-200px)] rounded-4xl bg-clip-padding bg-gradient-to-t from-[#FF00D0] to-[#FF1500] p-2 after:content-[''] after:absolute after:inset-0 after:rounded-4xl after:shadow-[0_0_10px_5px_rgba(255,21,0,0.5)]"
     >
-      <CardContent className="flex flex-col gap-8 h-full">
-        <div className="flex justify-start">
-          <h2 className="text-4xl font-extrabold after:content-[''] after:mt-3 after:block after:w-full after:h-1 after:bg-gradient-to-b after:from-[#FF1500] after:to-[#FF1500] after:blur-[2px]">IMPACT</h2>
-        </div>
-        <ul className="list-none space-y-2">
-          <li className="flex items-start gap-2">
-            <span className="text-red-500 blur-[2px]">🔴</span>
-            <span className="font-semibold">EU AI regulation aims to protect rights and ensure transparency but may slow innovation and increase costs. It could set a global standard while pushing companies to relocate. Public trust may grow, but businesses fear overregulation.</span>
-          </li>
-          <li className="flex items-start gap-2">
-            <span className="text-red-500 blur-[2px]">🔴</span>
-            <span className="font-semibold">Affected Groups: Tech companies and start-ups developing AI models</span>
-          </li>
-          <li className="flex items-start gap-2">
-            <span className="text-red-500 blur-[2px]">🔴</span>
-            <span className="font-semibold">Progressive/Left Position: Strong regulations are necessary to prevent discrimination and AI misuse.</span>
-          </li>
-        </ul>
+      <CardContent className="w-full h-full rounded-3xl bg-white p-6 flex flex-col gap-8">
+      <div className="flex justify-start">
+        <h2 className="text-4xl font-extrabold after:content-[''] after:mt-3 after:block after:w-full after:h-1 after:bg-gradient-to-b after:from-[#FF1500] after:to-[#FF1500] after:blur-[2px]">IMPACT</h2>
+      </div>
+      <ul className="list-none space-y-2">
+        <li className="flex items-start gap-2">
+        <span className="text-red-500 blur-[2px]">🔴</span>
+        <span className="font-semibold">EU AI regulation aims to protect rights and ensure transparency but may slow innovation and increase costs. It could set a global standard while pushing companies to relocate. Public trust may grow, but businesses fear overregulation.</span>
+        </li>
+        <li className="flex items-start gap-2">
+        <span className="text-red-500 blur-[2px]">🔴</span>
+        <span className="font-semibold">Affected Groups: Tech companies and start-ups developing AI models</span>
+        </li>
+        <li className="flex items-start gap-2">
+        <span className="text-red-500 blur-[2px]">🔴</span>
+        <span className="font-semibold">Progressive/Left Position: Strong regulations are necessary to prevent discrimination and AI misuse.</span>
+        </li>
+      </ul>
       </CardContent>
       <p className="absolute bottom-6 left-6">Source: <Link href={"https://www.theguardian.com/europe"} className="italic text-blue-500 underline">The Guardian</Link></p>
     </Card>
