5a06bfad3e027716f697789328aa957ee8e11c4c
Gontier <alexis.gontier03@gmail.com>
2025-03-20 03:14:32 +0100

    ✨ Add cards (Card1, Card2, Card3, Card4)

---
+++ components/scroll/cards/Card1.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CardTemplate from './CardTemplate'
+
+export default function Card1() {
+  return (
+    <CardTemplate shadowColor="blue">
+      <div>test</div>
+    </CardTemplate>
+  )
+}
---
+++ components/scroll/cards/Card2.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Card2() {
+  return (
+    <div className="h-card shadow-md">
+      <h2>CONTEXT</h2>
+      <p>
+        In response to escalating geopolitical tensions and security concerns, EU leaders have agreed to significantly increase defense expenditures. This move aims to enhance the EU's autonomous defense capabilities and reduce reliance on external powers.
+      </p>
+      <p>Source: The Guardian</p>
+    </div>
+  )
+}
