fdcae9715daeea13405e8abd50bcf7eb4cdedc07
Gontier <alexis.gontier03@gmail.com>
2025-03-20 03:35:15 +0100

    ✨ Update page component to use full height for better layout consistency

--- app/(article11)/page.tsx
+++ app/(article11)/page.tsx
@@ -2,7 +2,7 @@ import VerticalCarousel from "@/components/scroll/VerticalCarousel"
 
 export default function page() {
   return (
-    <div className="max-w-96 overflow-hidden m-auto max-h-screen">
+    <div className="max-w-96 overflow-hidden m-auto h-full">
       <VerticalCarousel />
     </div>
   )
