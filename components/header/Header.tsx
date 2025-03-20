a6d7d962cb4749f0bf4df4e17b036b9a7e759984
Gontier <alexis.gontier03@gmail.com>
2025-03-20 01:20:43 +0100

    ✨ Add aria-label

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,13 +7,14 @@ import { AvatarDemo } from "@/components/avatar/avatar";
 export default function Header() {
 
     const pathname = usePathname();
-    const pageName = pathname === "/" ? "Home" : pathname.substring(1).charAt(0)+ pathname.slice(2);
+    const pageName = pathname === "/" ? "Home" : pathname.split('/')[1];
 
     return (
         <div className="p-4 flex place-content-between">
             <h1 className="text-3xl font-extrabold uppercase">{pageName}</h1>
             <Link
                 href="/profile"
+                aria-label="Go to profile"
                 className="border-2 hover:border-blue-500 rounded-full transition duration-100 ease-in-out"
             >
                 <AvatarDemo />
