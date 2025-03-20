80b622b031408b138fdee4d74717c5c360996b79
Alexis G <alexis.gontier03@gmail.com>
2025-03-20 16:07:47 +0100

    ✨ Commented out the ProtectedRoute component in the dashboard layout

--- app/(article11)/layout.tsx
+++ app/(article11)/layout.tsx
@@ -1,15 +1,15 @@
-import ProtectedRoute from "@/components/auth/ProtectedRoute";
+// import ProtectedRoute from "@/components/auth/ProtectedRoute";
 import Header from "@/components/header/Header";
 import Navbar from "@/components/navbar/Navbar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <ProtectedRoute>
+            {/* <ProtectedRoute> */}
                 <Header />
                     <div className="px-8 pt-20 pb-20">{children}</div>
                 <Navbar />
-            </ProtectedRoute>
+            {/* </ProtectedRoute> */}
         </>
     )
 }
