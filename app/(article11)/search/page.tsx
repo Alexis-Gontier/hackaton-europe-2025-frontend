3e0fd7f212afb0d704d7c1b7b7c558200315631e
Gontier <alexis.gontier03@gmail.com>
2025-03-20 01:20:31 +0100

    ✨ Simplified search input container by removing unnecessary padding

--- app/(article11)/search/page.tsx
+++ app/(article11)/search/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input"
 
 export default function page() {
   return (
-    <div className="px-4 flex items-center gap-2">
+    <div className="flex items-center gap-2">
       <Input
         placeholder='Search'
         className='rounded-full border-1 border-black'
