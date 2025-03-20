9112fd46ec454cee17b031d93dc76d59ca48beae
Gontier <alexis.gontier03@gmail.com>
2025-03-20 01:20:06 +0100

    ✨ Added the DashboardLayout component and simplified the Login and Register pages by directly integrating the forms

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,11 +1,5 @@
 import RegisterForm from "@/components/auth/RegisterForm"
 
 export default function RegisterPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <RegisterForm />
-      </div>
-    </div>
-  )
+  return <RegisterForm />
 }
