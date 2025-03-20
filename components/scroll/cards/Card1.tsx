a698d2a88360bb6ad1f6403acf31ea87bf4a70bd
Gontier <alexis.gontier03@gmail.com>
2025-03-20 03:34:04 +0100

    ✨ Refactor Card1 and Card2 components to use new Card structure and improve layout

--- components/scroll/cards/Card1.tsx
+++ components/scroll/cards/Card1.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import CardTemplate from './CardTemplate'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Card1() {
   return (
-    <CardTemplate shadowColor="blue">
-      <div>test</div>
-    </CardTemplate>
-  )
+    <Card
+      className="relative w-full h-[calc(100vh-200px)] border-6 border-blue-500 rounded-4xl bg-[url('/icon-192x192.png')] bg-cover bg-center"
+    >
+      <CardContent className="flex flex-col items-center justify-center gap-4 h-full text-center transform translate-y-20">
+        <h2 className="text-3xl font-bold text-white">
+          EU Defense Spending Increase
+        </h2>
+        <div className="w-5/6 h-1 bg-blue-500 rounded-full"></div>
+        <p className="text-base text-white">The European Union plans a historic rise in defense spending to bolster its defense capabilities.</p>
+      </CardContent>
+      <p className="absolute bottom-6 left-6 text-white">Source: <Link href={"https://www.theguardian.com/europe"} className="italic text-blue-500 underline">The Guardian</Link></p>
+    </Card>
+  );
 }
--- components/scroll/cards/Card2.tsx
+++ components/scroll/cards/Card2.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Card2() {
   return (
-    <div className="h-card shadow-md">
-      <h2>CONTEXT</h2>
-      <p>
-        In response to escalating geopolitical tensions and security concerns, EU leaders have agreed to significantly increase defense expenditures. This move aims to enhance the EU's autonomous defense capabilities and reduce reliance on external powers.
-      </p>
-      <p>Source: The Guardian</p>
-    </div>
-  )
+    <Card
+      className="relative w-full h-[calc(100vh-200px)] border-6 border-green-400 rounded-4xl"
+    >
+      <CardContent className="flex flex-col items-center justify-center gap-4 h-full text-center">
+        <h2 className="text-3xl font-bold">
+          EU Defense Spending Increase
+        </h2>
+        <div className="w-5/6 h-1 bg-green-500 rounded-full"></div>
+        <p className="text-base">The European Union plans a historic rise in defense spending to bolster its defense capabilities.</p>
+      </CardContent>
+      <p className="absolute bottom-6 left-6">Source: <Link href={"https://www.theguardian.com/europe"} className="italic text-blue-500 underline">The Guardian</Link></p>
+    </Card>
+  );
 }
