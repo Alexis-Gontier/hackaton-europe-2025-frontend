9b56863af6c028d242323dfad294579d1b6f2144
Gontier <alexis.gontier03@gmail.com>
2025-03-21 01:47:10 +0100

    ✨ Added background blur effect and improved CSS class for Card3 component

--- components/scroll/cards/Card3.tsx
+++ components/scroll/cards/Card3.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 export default function Card3() {
   return (
     <Card
-      className="relative w-full h-[calc(100vh-200px)] rounded-4xl bg-clip-padding bg-gradient-to-t from-[#FF00D0] to-[#FF1500] p-2 after:content-[''] after:absolute after:inset-0 after:rounded-4xl after:shadow-[0_0_10px_5px_rgba(255,21,0,0.5)]"
+      className="relative w-full h-[calc(100vh-200px)] rounded-4xl bg-clip-padding bg-gradient-to-t from-[#FF00D0] to-[#FF1500] p-2 after:z-[-1] after:content-[''] after:absolute after:inset-0 after:rounded-4xl after:shadow-[0_0_10px_5px_rgba(255,21,0,0.5)]"
     >
-      <CardContent className="w-full h-full rounded-3xl bg-white p-6 flex flex-col gap-8">
+      <CardContent className="w-full h-full rounded-3xl bg-white p-6 flex flex-col gap-8 overflow-scroll">
       <div className="flex justify-start">
         <h2 className="text-4xl font-extrabold after:content-[''] after:mt-3 after:block after:w-full after:h-1 after:bg-gradient-to-b after:from-[#FF1500] after:to-[#FF1500] after:blur-[2px]">IMPACT</h2>
       </div>
@@ -28,7 +28,7 @@ export default function Card3() {
         </li>
       </ul>
       </CardContent>
-      <p className="absolute bottom-6 left-6">Source: <Link href={"https://www.theguardian.com/europe"} className="italic text-blue-500 underline">The Guardian</Link></p>
+      <p className="absolute bottom-6 left-6 backdrop-blur-xs">Source: <Link href={"https://www.theguardian.com/europe"} className="italic text-blue-500 underline">The Guardian</Link></p>
     </Card>
   );
 }
